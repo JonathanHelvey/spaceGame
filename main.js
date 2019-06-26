50dfdf4f725a12b4f051808d178d74315b4efa38
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-06-25 21:38:50 -0500

    borders

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let Scale = {
 //Holder array variables
 let ground = [];
 let houses = [];
-
+let stars = [];
 //Aliases
 let Application = PIXI.Application,
   loader = PIXI.loader,
@@ -43,8 +43,12 @@ let spaceShip, id, star;
 loader.add(["/assets/spaceship.png", "/assets/star.png"]).load(setup);
 
 function gameLoop(delta) {
-  //Move the cat 1 pixel
-  //spaceShip.x += 1;
+  // this is a workaround to enforce z ordering
+  // the player should be rendered above all particles
+  // remove the player
+  // add the player
+  app.stage.removeChild(spaceShip);
+  app.stage.addChild(spaceShip);
   state(delta);
 }
 
@@ -52,6 +56,13 @@ function play(delta) {
   //Use the cat's velocity to make it move
   spaceShip.x += spaceShip.vx;
   spaceShip.y += spaceShip.vy;
+
+  contain(spaceShip, {
+    x: 10,
+    y: 1,
+    width: Scale.width,
+    height: Scale.height
+  });
 }
 
 //This `setup` function will run when the image has loaded
@@ -70,7 +81,6 @@ function setup() {
   spaceShip.y = Scale.height / 2;
   spaceShip.anchor.set(0.5, 0.5);
   spaceShip.rotation = 0;
-
   spaceShip.vx = 0;
   spaceShip.vy = 0;
   spaceShip.hp = 10;
@@ -78,21 +88,40 @@ function setup() {
   //Add the spaceship to the stage
   app.stage.addChild(spaceShip);
 
+  //STARS
   let starSprite = new PIXI.Texture.fromImage("/assets/star.png");
-  let stars = [];
+  let numberOfStars = 100,
+    spacing = 48,
+    xOffset = 150,
+    speed = 2,
+    direction = 1;
   //Change the sprite's position
-  for (let i = 0; i < 100; i++) {
+
+  for (let i = 0; i < numberOfStars; i++) {
     star = new Sprite(starSprite);
-    const starRatio = (Scale.unit * 4) / 204;
-    star.scale.set(starRatio, starRatio);
-    star.x = Math.floor(Math.random() * 500 + 1);
-    star.y = Math.floor(Math.random() * 500 + 1);
+    let x = spacing * i + xOffset;
+    // let y = randomInt(0, app.stage.height - star.height);
+    const starRatio = (Scale.unit * 8) / 204;
+    star.scale.set(0.08 + Math.random() * 0.8);
+    star.x = Math.floor(Math.random() * app.screen.width);
+    star.y = Math.floor(Math.random() * app.screen.width);
     star.anchor.set(0.5, 0.5);
-    star.rotation = 0;
-    star.vx = 0;
-    star.vy = 0;
+    //star.rotation = 0;
+    star.vy = speed * direction;
+
+    //star.vy = 0;
+    star.tint = Math.random() * 0xffffff;
+    // create some extra properties that will control movement :
+    // create a random direction in radians. This is a number between 0 and PI*2 which is the equivalent of 0 - 360 degrees
+    star.direction = Math.random() * Math.PI * 2;
+    // this number will be used to modify the direction of the dude over time
+    star.turningSpeed = Math.random() - 0.8;
+    // create a random speed for the dude between 0 - 2
+    star.speed = 2 + Math.random() * 2;
+
+    // finally we push the stars into the array so it it can be easily accessed later
     stars.push(star);
-    //Add the spaceship to the stage
+    //Add the star to the stage
     app.stage.addChild(star);
   }
   //Start the game loop by adding the `gameLoop` function to
@@ -114,7 +143,7 @@ function shoot(rotation, startPosition) {
   bullet.position.y = startPosition.y;
   bullet.position.x = startPosition.x;
   bullet.rotation = rotation - 1.6;
-  bullet.anchor.set(0.5, 1);
+  bullet.anchor.set(0.5, 0.8);
   bullet.hp = 1;
   bullets.push(bullet);
   app.stage.addChild(bullet);
@@ -141,28 +170,28 @@ animate();
 let downListener = event => {
   if (event.code === "KeyW") {
     if (spaceShip.vy > -1) {
-      spaceShip.vy -= 2.5;
+      spaceShip.vy -= 3.5;
       spaceShip.vx = 0;
       spaceShip.rotation = 0;
     }
   }
   if (event.code === "KeyA") {
     if (spaceShip.vx > -1) {
-      spaceShip.vx -= 2.5;
+      spaceShip.vx -= 3.5;
       spaceShip.vy = 0;
       spaceShip.rotation = 0;
     }
   }
   if (event.code === "KeyS") {
     if (spaceShip.vy < 1) {
-      spaceShip.vy += 2.5;
+      spaceShip.vy += 3.5;
       spaceShip.vx = 0;
       spaceShip.rotation = 0;
     }
   }
   if (event.code === "KeyD") {
     if (spaceShip.vx < 1) {
-      spaceShip.vx += 2.5;
+      spaceShip.vx += 3.5;
       spaceShip.vy = 0;
       spaceShip.rotation = 0;
     }
@@ -192,3 +221,36 @@ let upListener = event => {
 window.addEventListener("keydown", downListener, false);
 
 window.addEventListener("keyup", upListener, false);
+
+//Helper Functions
+
+function contain(sprite, container) {
+  let collision = undefined;
+
+  //Left
+  if (sprite.x < container.x) {
+    sprite.x = container.x;
+    collision = "left";
+  }
+
+  //Top
+  if (sprite.y < container.y) {
+    sprite.y = container.y;
+    collision = "top";
+  }
+
+  //Right
+  if (sprite.x + sprite.width > container.width) {
+    sprite.x = container.width - sprite.width;
+    collision = "right";
+  }
+
+  //Bottom
+  if (sprite.y + sprite.height > container.height) {
+    sprite.y = container.height - sprite.height;
+    collision = "bottom";
+  }
+
+  //Return the `collision` value
+  return collision;
+}
